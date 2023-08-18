31fb075c410b5ad89482519ec6fe2e6396eff5f8
Eunnyeong <doseee114@gmail.com>
2023-08-18 18:32:04 +0900

    FEAT: CORS 설정 파일 추가

---
+++ backend/src/main/java/com/kb/report/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.kb.report.config;
+import java.util.Arrays;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+  @Bean
+  public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+
+    config.setAllowedOriginPatterns(Arrays.asList("*"));
+    config.setAllowedMethods(Arrays.asList("HEAD","POST","GET","DELETE","PATCH"));
+    config.setAllowedHeaders(Arrays.asList("*"));
+
+    source.registerCorsConfiguration("/**", config);
+    return new CorsFilter(source);
+  }
+}
